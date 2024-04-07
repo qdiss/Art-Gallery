107bfde4f7673e4985e7923b8f92b26ff7b010d3
Klobodanovic Adis <klobodanovic.adis.21@size.ba>
2024-04-07 19:41:27 +0200

    hero

--- components/shared/carouselMain.tsx
+++ components/shared/carouselMain.tsx
@@ -6,17 +6,17 @@ import { Transition } from "@headlessui/react";
 import MainHero from "./hero";
 
 export default function Carousel() {
-  const [activeCarousel, setActiveCarousel] = useState<string>("bestWork");
+  const [activeCarousel, setActiveCarousel] = useState<string>("hero");
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setActiveCarousel((prevActive: string) => {
-  //       return prevActive === "hero" ? "bestWork" : "hero";
-  //     });
-  //   }, 5000); //Promenite broj u milisekundama prema potrebi (ovde svakih 10 sekundi)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveCarousel((prevActive: string) => {
+        return prevActive === "hero" ? "bestWork" : "hero";
+      });
+    }, 5000); //Promenite broj u milisekundama prema potrebi (ovde svakih 10 sekundi)
 
-  //   return () => clearInterval(interval);
-  // }, []); //Prazno polje kao zavisnost osigurava da se useEffect izvršava samo prilikom montiranja komponente
+    return () => clearInterval(interval);
+  }, []); //Prazno polje kao zavisnost osigurava da se useEffect izvršava samo prilikom montiranja komponente
 
   return (
     <div className="relative">
